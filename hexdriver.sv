ad63b938026b02530792b1a842b8c0d9fe534df2
Andrew Boland <65364090+agboland@users.noreply.github.com>
2023-09-22 16:06:04 -0400

    Update hexdriver.sv
    #2 was showing as a 3 on 7 seg
    #3 was showing as a 7 on 7 seg

--- hexdriver.sv
+++ hexdriver.sv
@@ -6,8 +6,8 @@ module hexdriver (input [3:0] val, output logic [6:0] HEX);
 			// abc_defg
 			4'b0000: HEX = 7'b100_0000;
 			4'b0001: HEX = 7'b111_1001;
-			4'b0010: HEX = 7'b011_0000;
-			4'b0011: HEX = 7'b101_1000;
+			4'b0010: HEX = 7'b010_0100;
+			4'b0011: HEX = 7'b011_0000;
 			4'b0100: HEX = 7'b001_1001;
 			4'b0101: HEX = 7'b001_0010;
 			4'b0110: HEX = 7'b000_0010;
