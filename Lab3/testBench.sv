228c32cab2b60b0024c014de0fa325b00e6f0e78
Andrew Boland <65364090+agboland@users.noreply.github.com>
2023-10-30 13:22:09 -0400

    Add files via upload

--- Lab3/testBench.sv
+++ Lab3/testBench.sv
@@ -39,7 +39,6 @@ module testbench;
 				parameter FUNCT3_ADD = 3'b000;
 				parameter FUNCT7_ADD = 7'b0000000;
 				
-			
 				//sub
 				parameter FUNCT3_SUB = 3'b000;
 				parameter FUNCT7_SUB = 7'b0100000;
@@ -88,6 +87,8 @@ module testbench;
 				parameter FUNCT3_MULHU = 3'b011;
 				parameter FUNCT7_MULHU = 7'b0000001;
 			
+			parameter OPCODE_CSRRW = 7'b1110011
+			
 				//CSSRW
 				parameter FUNCT3_CSSRW = 3'b111;
 			
@@ -133,12 +134,14 @@ module testbench;
         test_count = 0;
         done = 0;
 
-        // Test case loop
+        // Test case loop 
         while (!done) begin
 				rs1 = 5'd1;
 				rs2 = 5'd2;
 				rd = 5'd3;
 		  
+
+		  		//ADD TEST
 				rs1_val = 32'h00000001;
 				rs2_val = 32'h00000001;
 				regfile[rs1] = rs1_val;
@@ -160,8 +163,434 @@ module testbench;
             // Increment the test count
             test_count = test_count + 1;
 				
-				
-            // Optionally, add more test cases
+
+			//SUB TEST
+			rs1_val = 32'h00000002;
+			rs2_val = 32'h00000001;
+
+			expected_result = rs1_val - rs2_val;
+
+			#10;
+			
+            // Check the result
+            if (result !== expected_result) begin
+                $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+                done = 1;
+            end				
+
+            // Increment the test count
+            test_count = test_count + 1;            
+
+
+		//AND TEST
+rs1_val = 32'h0000000F;
+rs2_val = 32'h00000003;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+AND_instruction = {FUNCT7_AND, rs2, rs1, FUNCT3_AND, rd, OPCODE_RTYPE}
+
+expected_result = rs1_val & rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//OR TEST
+rs1_val = 32'h0000000F;
+rs2_val = 32'h00000003;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+OR_instruction = {FUNCT7_OR, rs2, rs1, FUNCT3_OR, rd, OPCODE_RTYPE}
+
+expected_result = rs1_val | rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//XOR TEST
+rs1_val = 32'h0000000F;
+rs2_val = 32'h00000003;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+XOR_instruction = {FUNCT7_XOR, rs2, rs1, FUNCT3_XOR, rd, OPCODE_RTYPE}
+
+expected_result = rs1_val ^ rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SLL TEST
+rs1_val = 32'h00000004;
+rs2_val = 32'h00000003; // Shifting by 3 bits
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+SLL_instruction = {FUNCT7_SLL, rs2, rs1, FUNCT3_SLL, rd, OPCODE_RTYPE}
+
+expected_result = rs1_val << rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+
+//SRA TEST
+rs1_val = 32'hF0000004; // A negative number to show arithmetic shift
+rs2_val = 32'h00000003; // Shifting by 3 bits
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+SRA_instruction = {FUNCT7_SRA, rs2, rs1, FUNCT3_SRA, rd, OPCODE_RTYPE}
+
+expected_result = $signed(rs1_val) >>> rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SRL TEST
+rs1_val = 32'hF0000004;
+rs2_val = 32'h00000003; // Shifting by 3 bits
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+SRL_instruction = {FUNCT7_SRL, rs2, rs1, FUNCT3_SRL, rd, OPCODE_RTYPE}
+
+expected_result = rs1_val >>> rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SLT TEST
+rs1_val = 32'h00000004;
+rs2_val = 32'h00000003;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+SLT_instruction = {FUNCT7_SLT, rs2, rs1, FUNCT3_SLT, rd, OPCODE_RTYPE}
+
+expected_result = ($signed(rs1_val) < $signed(rs2_val)) ? 1 : 0;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SLTU TEST
+rs1_val = 32'h00000004;
+rs2_val = 32'h00000003;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+SLTU_instruction = {FUNCT7_SLTU, rs2, rs1, FUNCT3_SLTU, rd, OPCODE_RTYPE}
+
+expected_result = (rs1_val < rs2_val) ? 1 : 0;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//MUL TEST
+rs1_val = 32'h00000004;
+rs2_val = 32'h00000003;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+MUL_instruction = {FUNCT7_MUL, rs2, rs1, FUNCT3_MUL, rd, OPCODE_RTYPE}
+
+expected_result = rs1_val * rs2_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//MULH TEST
+rs1_val = 32'h0000FFFF;
+rs2_val = 32'h0000FFFF;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+MULH_instruction = {FUNCT7_MULH, rs2, rs1, FUNCT3_MULH, rd, OPCODE_RTYPE}
+
+expected_result = ($signed(rs1_val) * $signed(rs2_val)) >>> 32;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//MULHU TEST
+rs1_val = 32'h0000FFFF;
+rs2_val = 32'h0000FFFF;
+regfile[rs1] = rs1_val;
+regfile[rs2] = rs2_val;
+
+MULHU_instruction = {FUNCT7_MULHU, rs2, rs1, FUNCT3_MULHU, rd, OPCODE_RTYPE}
+
+expected_result = (rs1_val * rs2_val) >>> 32;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//CSRRW TEST
+csr_address = 32'h000000C0; // An example CSR address
+rs1_val = 32'h0000000A; // Value to write to CSR
+regfile[rs1] = rs1_val;
+
+CSRRW_instruction = {csr_address, rs1, FUNCT3_CSRRW, rd, OPCODE_CSRRW}
+
+expected_result = csr_read(csr_address); // Assuming csr_read is a function that reads CSR
+
+#10;
+
+// Check the result and also check if the CSR is updated
+if (result !== expected_result || csr_read(csr_address) !== rs1_val) begin
+    $display("Test case %d failed: Expected %h, Got %h or CSR not updated", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//ADDI TEST
+rs1_val = 32'h00000004;
+imm_val = 32'h00000003; // Immediate value
+regfile[rs1] = rs1_val;
+
+ADDI_instruction = {imm_val, rs1, FUNCT3_ADDI, rd, OPCODE_ITYPE}
+
+expected_result = rs1_val + imm_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//ANDI TEST
+rs1_val = 32'h0000000F;
+imm_val = 32'h00000003; // Immediate value
+regfile[rs1] = rs1_val;
+
+ANDI_instruction = {imm_val, rs1, FUNCT3_ANDI, rd, OPCODE_ITYPE}
+
+expected_result = rs1_val & imm_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//ORI TEST
+rs1_val = 32'h0000000F;
+imm_val = 32'h00000003; // Immediate value
+regfile[rs1] = rs1_val;
+
+ORI_instruction = {imm_val, rs1, FUNCT3_ORI, rd, OPCODE_ITYPE}
+
+expected_result = rs1_val | imm_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//XORI TEST
+rs1_val = 32'h0000000F;
+imm_val = 32'h00000003; // Immediate value
+regfile[rs1] = rs1_val;
+
+XORI_instruction = {imm_val, rs1, FUNCT3_XORI, rd, OPCODE_ITYPE}
+
+expected_result = rs1_val ^ imm_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SLLI TEST
+rs1_val = 32'h0000000F;
+shamt_val = 3; // Shift amount
+regfile[rs1] = rs1_val;
+
+SLLI_instruction = {shamt_val, rs1, FUNCT3_SLLI, rd, OPCODE_ITYPE}
+
+expected_result = rs1_val << shamt_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SRAI TEST
+rs1_val = 32'hFFFFFFF0;
+shamt_val = 4; // Shift amount
+regfile[rs1] = rs1_val;
+
+SRAI_instruction = {FUNCT7_SRAI, shamt_val, rs1, FUNCT3_SRAI, rd, OPCODE_ITYPE}
+
+expected_result = $signed(rs1_val) >>> shamt_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//SRLI TEST
+rs1_val = 32'hFFFFFFFF;
+shamt_val = 4; // Shift amount
+regfile[rs1] = rs1_val;
+
+SRLI_instruction = {shamt_val, rs1, FUNCT3_SRLI, rd, OPCODE_ITYPE}
+
+expected_result = rs1_val >>> shamt_val;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
+
+//LUI TEST
+imm_val = 32'h0000F000; // Immediate value
+
+LUI_instruction = {imm_val, rd, OPCODE_LUI}
+
+expected_result = imm_val << 12;
+
+#10;
+
+// Check the result
+if (result !== expected_result) begin
+    $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
+    done = 1;
+end
+
+// Increment the test count
+test_count = test_count + 1;
 
 
             // If all tests are done, stop the simulation
