c4091828c874a92396f4c4900855fd28f1afc07f
Andrew Boland <65364090+agboland@users.noreply.github.com>
2023-12-07 11:28:29 -0500

    Fixed compile errors
    Should test instructions only will be testing modules next and I will clean up later. this thing looks nasty

--- Lab3/testBench.sv
+++ Lab3/testBench.sv
@@ -3,7 +3,7 @@ module testbench;
 
     // Define parameters for test case configuration
     parameter NUM_TESTS = 10;
-    parameter INITIAL_PC = 0x1000; // Initial program counter value
+    parameter INITIAL_PC = 32'h1000; // Initial program counter value
     parameter TEST_TIMEOUT = 1000; // Timeout in simulation steps
 
     // Declare signals and variables
@@ -12,6 +12,10 @@ module testbench;
     wire [31:0] result;
     reg [31:0] instruction;
     reg [5:0] test_count;
+    reg [31:0] regfile[0:31];
+    reg [11:0] address_input; 
+    reg [11:0] imm_val;
+    reg [4:0] shamt_val;
     reg done;
 
     // Instantiate the CPU and other components
@@ -86,15 +90,14 @@ module testbench;
 				//MULHU
 				parameter FUNCT3_MULHU = 3'b011;
 				parameter FUNCT7_MULHU = 7'b0000001;
-			
-			parameter OPCODE_CSRRW = 7'b1110011
-			
+
 				//CSSRW
-				parameter FUNCT3_CSSRW = 3'b111;
+				parameter OPCODE_CSRRW = 7'b1110011;
+				parameter FUNCT3_CSSRW = 3'b001;
 			
 			
 			//I-Type opcode
-			parameter OPCODE_ITYPE = 7'b0010011
+			parameter OPCODE_ITYPE = 7'b0010011;
 			
 				//ADDI
 				parameter FUNCT3_ADDI = 3'b000;
@@ -136,21 +139,22 @@ module testbench;
 
         // Test case loop 
         while (!done) begin
-				rs1 = 5'd1;
-				rs2 = 5'd2;
-				rd = 5'd3;
+			rs1 = 5'd1;
+			rs2 = 5'd2;
+			rd = 5'd3;
 		  
 
-		  		//ADD TEST
-				rs1_val = 32'h00000001;
-				rs2_val = 32'h00000001;
-				regfile[rs1] = rs1_val;
-				regfile[rs2] = rs2_val;
+		  	//ADD TEST
+			rs1_val = 32'h00000001;
+			rs2_val = 32'h00000001;
+			regfile[rs1] = rs1_val;
+			regfile[rs2] = rs2_val;
+
 				
-				ADD_instruction = {FUNCT7_ADD, rs2, rs1, FUNCT3_ADD, rd, OPCODE_RTYPE}
+			instruction = {FUNCT7_ADD, rs2, rs1, FUNCT3_ADD, rd, OPCODE_RTYPE};
 				
 				
-				expected_result = rs1_val + rs2_val;
+			expected_result = rs1_val + rs2_val;
 				
             #10;
 
@@ -167,12 +171,16 @@ module testbench;
 			//SUB TEST
 			rs1_val = 32'h00000002;
 			rs2_val = 32'h00000001;
+			regfile[rs1] = rs1_val;
+			regfile[rs2] = rs2_val;
+
+			instruction = {FUNCT7_SUB, rs2, rs1, FUNCT3_SUB, rd, OPCODE_RTYPE};			
 
 			expected_result = rs1_val - rs2_val;
 
 			#10;
 			
-            // Check the result
+            // Check the result** Error: C:/Users/tacob/Downloads/CSCE-611-main/CSCE-611-main/Lab3/testBench.sv(150): (vlog-2730) Undefined variable: 'ADD_instruction'.
             if (result !== expected_result) begin
                 $display("Test case %d failed: Expected %h, Got %h", test_count, expected_result, result);
                 done = 1;
@@ -188,7 +196,7 @@ rs2_val = 32'h00000003;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-AND_instruction = {FUNCT7_AND, rs2, rs1, FUNCT3_AND, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_AND, rs2, rs1, FUNCT3_AND, rd, OPCODE_RTYPE};
 
 expected_result = rs1_val & rs2_val;
 
@@ -209,7 +217,7 @@ rs2_val = 32'h00000003;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-OR_instruction = {FUNCT7_OR, rs2, rs1, FUNCT3_OR, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_OR, rs2, rs1, FUNCT3_OR, rd, OPCODE_RTYPE};
 
 expected_result = rs1_val | rs2_val;
 
@@ -230,7 +238,7 @@ rs2_val = 32'h00000003;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-XOR_instruction = {FUNCT7_XOR, rs2, rs1, FUNCT3_XOR, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_XOR, rs2, rs1, FUNCT3_XOR, rd, OPCODE_RTYPE};
 
 expected_result = rs1_val ^ rs2_val;
 
@@ -251,7 +259,7 @@ rs2_val = 32'h00000003; // Shifting by 3 bits
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-SLL_instruction = {FUNCT7_SLL, rs2, rs1, FUNCT3_SLL, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_SLL, rs2, rs1, FUNCT3_SLL, rd, OPCODE_RTYPE};
 
 expected_result = rs1_val << rs2_val;
 
@@ -273,7 +281,7 @@ rs2_val = 32'h00000003; // Shifting by 3 bits
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-SRA_instruction = {FUNCT7_SRA, rs2, rs1, FUNCT3_SRA, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_SRA, rs2, rs1, FUNCT3_SRA, rd, OPCODE_RTYPE};
 
 expected_result = $signed(rs1_val) >>> rs2_val;
 
@@ -294,7 +302,7 @@ rs2_val = 32'h00000003; // Shifting by 3 bits
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-SRL_instruction = {FUNCT7_SRL, rs2, rs1, FUNCT3_SRL, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_SRL, rs2, rs1, FUNCT3_SRL, rd, OPCODE_RTYPE};
 
 expected_result = rs1_val >>> rs2_val;
 
@@ -315,7 +323,7 @@ rs2_val = 32'h00000003;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-SLT_instruction = {FUNCT7_SLT, rs2, rs1, FUNCT3_SLT, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_SLT, rs2, rs1, FUNCT3_SLT, rd, OPCODE_RTYPE};
 
 expected_result = ($signed(rs1_val) < $signed(rs2_val)) ? 1 : 0;
 
@@ -336,7 +344,7 @@ rs2_val = 32'h00000003;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-SLTU_instruction = {FUNCT7_SLTU, rs2, rs1, FUNCT3_SLTU, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_SLTU, rs2, rs1, FUNCT3_SLTU, rd, OPCODE_RTYPE};
 
 expected_result = (rs1_val < rs2_val) ? 1 : 0;
 
@@ -357,7 +365,7 @@ rs2_val = 32'h00000003;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-MUL_instruction = {FUNCT7_MUL, rs2, rs1, FUNCT3_MUL, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_MUL, rs2, rs1, FUNCT3_MUL, rd, OPCODE_RTYPE};
 
 expected_result = rs1_val * rs2_val;
 
@@ -378,7 +386,7 @@ rs2_val = 32'h0000FFFF;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-MULH_instruction = {FUNCT7_MULH, rs2, rs1, FUNCT3_MULH, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_MULH, rs2, rs1, FUNCT3_MULH, rd, OPCODE_RTYPE};
 
 expected_result = ($signed(rs1_val) * $signed(rs2_val)) >>> 32;
 
@@ -399,7 +407,7 @@ rs2_val = 32'h0000FFFF;
 regfile[rs1] = rs1_val;
 regfile[rs2] = rs2_val;
 
-MULHU_instruction = {FUNCT7_MULHU, rs2, rs1, FUNCT3_MULHU, rd, OPCODE_RTYPE}
+instruction = {FUNCT7_MULHU, rs2, rs1, FUNCT3_MULHU, rd, OPCODE_RTYPE};
 
 expected_result = (rs1_val * rs2_val) >>> 32;
 
@@ -415,18 +423,18 @@ end
 test_count = test_count + 1;
 
 //CSRRW TEST
-csr_address = 32'h000000C0; // An example CSR address
+address_input = 32'h000000C0; // An example CSR address
 rs1_val = 32'h0000000A; // Value to write to CSR
 regfile[rs1] = rs1_val;
 
-CSRRW_instruction = {csr_address, rs1, FUNCT3_CSRRW, rd, OPCODE_CSRRW}
+instruction = {address_input, rs1, FUNCT3_CSSRW, rd, OPCODE_CSRRW};
 
-expected_result = csr_read(csr_address); // Assuming csr_read is a function that reads CSR
+expected_result = csr_read(address_input); // Assuming csr_read is a function that reads CSR
 
 #10;
 
 // Check the result and also check if the CSR is updated
-if (result !== expected_result || csr_read(csr_address) !== rs1_val) begin
+if (result !== expected_result || csr_read(address_input) !== rs1_val) begin
     $display("Test case %d failed: Expected %h, Got %h or CSR not updated", test_count, expected_result, result);
     done = 1;
 end
@@ -439,7 +447,7 @@ rs1_val = 32'h00000004;
 imm_val = 32'h00000003; // Immediate value
 regfile[rs1] = rs1_val;
 
-ADDI_instruction = {imm_val, rs1, FUNCT3_ADDI, rd, OPCODE_ITYPE}
+instruction = {imm_val, rs1, FUNCT3_ADDI, rd, OPCODE_ITYPE};
 
 expected_result = rs1_val + imm_val;
 
@@ -459,7 +467,7 @@ rs1_val = 32'h0000000F;
 imm_val = 32'h00000003; // Immediate value
 regfile[rs1] = rs1_val;
 
-ANDI_instruction = {imm_val, rs1, FUNCT3_ANDI, rd, OPCODE_ITYPE}
+instruction = {imm_val, rs1, FUNCT3_ANDI, rd, OPCODE_ITYPE};
 
 expected_result = rs1_val & imm_val;
 
@@ -479,7 +487,7 @@ rs1_val = 32'h0000000F;
 imm_val = 32'h00000003; // Immediate value
 regfile[rs1] = rs1_val;
 
-ORI_instruction = {imm_val, rs1, FUNCT3_ORI, rd, OPCODE_ITYPE}
+instruction = {imm_val, rs1, FUNCT3_ORI, rd, OPCODE_ITYPE};
 
 expected_result = rs1_val | imm_val;
 
@@ -499,7 +507,7 @@ rs1_val = 32'h0000000F;
 imm_val = 32'h00000003; // Immediate value
 regfile[rs1] = rs1_val;
 
-XORI_instruction = {imm_val, rs1, FUNCT3_XORI, rd, OPCODE_ITYPE}
+instruction = {imm_val, rs1, FUNCT3_XORI, rd, OPCODE_ITYPE};
 
 expected_result = rs1_val ^ imm_val;
 
@@ -519,7 +527,7 @@ rs1_val = 32'h0000000F;
 shamt_val = 3; // Shift amount
 regfile[rs1] = rs1_val;
 
-SLLI_instruction = {shamt_val, rs1, FUNCT3_SLLI, rd, OPCODE_ITYPE}
+instruction = {shamt_val, rs1, FUNCT3_SLLI, rd, OPCODE_ITYPE};
 
 expected_result = rs1_val << shamt_val;
 
@@ -539,7 +547,7 @@ rs1_val = 32'hFFFFFFF0;
 shamt_val = 4; // Shift amount
 regfile[rs1] = rs1_val;
 
-SRAI_instruction = {FUNCT7_SRAI, shamt_val, rs1, FUNCT3_SRAI, rd, OPCODE_ITYPE}
+instruction = {FUNCT7_SRAI, shamt_val, rs1, FUNCT3_SRAI, rd, OPCODE_ITYPE};
 
 expected_result = $signed(rs1_val) >>> shamt_val;
 
@@ -556,10 +564,10 @@ test_count = test_count + 1;
 
 //SRLI TEST
 rs1_val = 32'hFFFFFFFF;
-shamt_val = 4; // Shift amount
+shamt_val = 4'b0100; // Shift amount
 regfile[rs1] = rs1_val;
 
-SRLI_instruction = {shamt_val, rs1, FUNCT3_SRLI, rd, OPCODE_ITYPE}
+instruction = {FUNCT7_SRLI, shamt_val, rs1, FUNCT3_SRLI, rd, OPCODE_ITYPE};
 
 expected_result = rs1_val >>> shamt_val;
 
@@ -576,8 +584,8 @@ test_count = test_count + 1;
 
 //LUI TEST
 imm_val = 32'h0000F000; // Immediate value
-
-LUI_instruction = {imm_val, rd, OPCODE_LUI}
+regfile[imm_val] = imm_val;
+instruction = {imm_val, rd, OPCODE_UTYPE};
 
 expected_result = imm_val << 12;
 
@@ -591,7 +599,6 @@ end
 
 // Increment the test count
 test_count = test_count + 1;
-
 
             // If all tests are done, stop the simulation
             if (test_count >= NUM_TESTS) begin
