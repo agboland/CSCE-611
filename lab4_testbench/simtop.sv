dc7e18bd77df9d6636bf431832fcda32711c9f19
Andrew Boland <65364090+agboland@users.noreply.github.com>
2023-12-14 11:18:55 -0500

    Update simtop.sv
    Error checking. Just need to add comb logic from control unit

--- lab4_testbench/simtop.sv
+++ lab4_testbench/simtop.sv
@@ -9,22 +9,23 @@ module simtop;
     logic rst;
 	 
 	 // Probing CPU for test bench
-    wire [31:0] inst_mem_out;
-	 wire [4:0]  regdest_WB_out;
-	 wire 		 regwrite_WB_out;
-	 wire [1:0]  regselWB_reg_out;
-	 wire [31:0] GPIOoutreg_out;
-	 wire [31:0] GPIOin_reg_out;
-	 wire [31:0] Rwb_reg_out;
-	 wire [31:0] IMM_WB_reg_out;
-	 wire [11:0] PC_EX_out;
-    wire        stall_EX_out;
 
-    wire  [31:0] dummy;
-    wire  [31:0] GPIOin_manual;
-    logic [11:0] address_input; 
-    logic [11:0] imm_val;
-    logic [4:0] shamt_val;
+    	wire stall_EX_out;
+	wire GPIO_we_out;
+	wire alusrc_EX_out;
+	wire regwrite_EX_out;
+	wire [1:0] regsel_EX_out;
+	wire [3:0] aluop_EX_out;
+	wire gpio_we_out;
+	wire [1:0] pcsrc_EX_out;
+	wire stall_FETCH_out;
+	wire [6:0] funct7_out;
+	wire [11:0] imm12_out;
+	wire [2:0] funct3_out;
+	wire [6:0] opcode_out;
+
+
+	
     logic done;
 
 	 assign GPIOin_manual = 32'd3;
@@ -33,20 +34,19 @@ module simtop;
     CPU cpu_inst (
         .clk(clk),
         .rst_n(rst),
-        .GPIO_in(GPIOin_manual),
-        .GPIO_out(dummy),
 		  
 		  // Test bench probing
-		  .inst_mem_out(inst_mem_out),
-		  .regdest_WB_out(regdest_WB_out),		//add 	- 	done
-		  .regwrite_WB_out(regwrite_WB_out),	//add 	- 	done
-		  .regselWB_reg_out(regselWB_reg_out),	//add 	- 	done
-		  .GPIOoutreg_out(GPIOoutreg_out),		//csrrw 	- 	done
-		  .GPIOin_reg_out(GPIOin_reg_out),		//csrrw 	-	done
-		  .Rwb_reg_out(Rwb_reg_out), 				//add 	- 	done
-		  .IMM_WB_reg_out(IMM_WB_reg_out), 		//lui 	- 	done
-		  .stall_EX_out(stall_EX_out), 			//jal		-	
-		  .PC_EX_out(PC_EX_out)						//add		-	
+
+	    .stall_EX_out(stall_EX_out), 			//jal		-	
+	    .PC_EX_out(PC_EX_out),//add		-	
+	    .GPIO_we_out(GPIO_we_ou),
+	    .alusrc_EX_out(alusrc_EX_out),
+	    .regwrite_EX_out(regwrite_EX_out),
+	    .regsel_EX_out(regsel_EX_out),
+	    .aluop_EX_out(aluop_EX_out),
+	    .gpio_we_out(gpio_we_out),
+	    .pcsrc_EX_out(pcsrc_EX_out),
+	    .stall_FETCH_out(stall_FETCH_out)
   );
 	 
 
@@ -56,12 +56,37 @@ module simtop;
 		initial begin
 			clk = 0; #5; // Set clock to 0
 			rst = 0; #5; // Enable sync reset
+			
 		repeat(2) begin
 			clk = ~clk; #5; // Cycle the clock once
 		end
 		rst = 1; #5; // Disable sync reset			
 
-		//ClK to load first instruction 
+ always @(posedge clk) begin
+        if (rst == 1) begin  // Only check after reset is deasserted
+            // Compare actual outputs with expected outputs
+            if (alusrc_EX_out != expected_alusrc_EX) begin
+                $display("Error: alusrc_EX_out incorrect at time %t", $time);
+            end
+            if (regwrite_EX_out != expected_regwrite_EX) begin
+                $display("Error: regwrite_EX_out incorrect at time %t", $time);
+            end
+            if (aluop_EX_out != expected_aluop_EX) begin
+                $display("Error: aluop_EX_out incorrect at time %t", $time);
+            end
+            if (pcsrc_EX_out != expected_pcsrc_EX) begin
+                $display("Error: pcsrc_EX_out incorrect at time %t", $time);
+            end
+            if (stall_FETCH_out != expected_stall_FETCH) begin
+                $display("Error: stall_FETCH_out incorrect at time %t", $time);
+            end
+            if (gpio_we_out != expected_gpio_we) begin
+                $display("Error: stall_FETCH_out incorrect at time %t", $time);
+            end
+        end
+    end
+			
+
 		repeat(10) begin
 			clk = ~clk; #5; // Cycle Clock
 		end
